00db03bbc1de02ee8abc5abc9d42845a67842310
miriamhsu123 <71687830+miriamhsu123@users.noreply.github.com>
2020-12-15 15:59:53 +0800

    npc碰撞接觸設定 對話內容顯示設定

---
+++ New Unity Project/Assets/Scripts/NPC.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialogue;
+    [Header("對話內容")]
+    public Text textContent;
+
+    public bool playerInArea;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            playerInArea = true;
+            Dialogue();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            playerInArea = false;
+        }
+    }
+    private void Dialogue()
+    {
+        for (int n = 0; n < data.dialogueA.Length; n++)
+        {
+            print(data.dialogueA[n]);
+        }    
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/NPCData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC 資料", menuName = "mo/NPC 資料")]
+public class NPCData : ScriptableObject
+{
+    [Header("第一段對話"),TextArea(1,5)]
+    public string dialogueA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialogueB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialogueC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("第一段對話")]
+    public int countCurrent;
+}
