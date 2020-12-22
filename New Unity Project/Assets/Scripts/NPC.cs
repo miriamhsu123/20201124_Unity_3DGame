f34f9cb54ca2298ea846c262aee40cfb6b2b3b38
miriamhsu123 <71687830+miriamhsu123@users.noreply.github.com>
2020-12-22 15:57:13 +0800

    NPC對話設定

--- New Unity Project/Assets/Scripts/NPC.cs
+++ New Unity Project/Assets/Scripts/NPC.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class NPC : MonoBehaviour
 {
@@ -9,14 +10,48 @@ public class NPC : MonoBehaviour
     public GameObject dialogue;
     [Header("對話內容")]
     public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.1f;
 
     public bool playerInArea;
+
+    //自定義列舉
+    public enum NPCState
+    {
+        FirstDialogue,Progressing,Finish //會有這三種狀態
+    }
+
+    //public 列舉之名稱 state = 列舉之名稱.列舉之狀態
+    //預設列舉狀態為
+    public NPCState state = NPCState.FirstDialogue;
+
+    /* 協同程序
+    private void Start()
+    {
+        //啟動協程
+        StartCoroutine(Test());
+    }
+
+    private IEnumerator Test()
+    {
+        print("first_second");
+        yield return new WaitForSeconds(1f);
+        print("second_second");
+        yield return new WaitForSeconds(2f);
+        print("forth_second");
+    }
+    */
+
+  
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "小明")
         {
             playerInArea = true;
-            Dialogue();
+            StartCoroutine (Dialogue());
         }
     }
     private void OnTriggerExit(Collider other)
@@ -24,13 +59,48 @@ public class NPC : MonoBehaviour
         if (other.name == "小明")
         {
             playerInArea = false;
+            StopDialogue();
         }
     }
-    private void Dialogue()
+
+    /// <summary>
+    /// 以下是"停止對話"
+    /// </summary>
+    private void StopDialogue()
     {
-        for (int n = 0; n < data.dialogueA.Length; n++)
+        dialogue.SetActive(false);  //關閉對話框
+        StopAllCoroutines();  //停止所有協程
+    }
+    private IEnumerator Dialogue()  //對話協程
+    {
+        dialogue.SetActive(true); //出現對話框
+        textContent.text = ""; //清空對話框
+
+        textName.text = name;
+
+        string dialogueString = data.dialogueB;
+
+        switch (state)
         {
-            print(data.dialogueA[n]);
+            case NPCState.FirstDialogue:
+                dialogueString = data.dialogueA;
+                break;
+            case NPCState.Progressing:
+                dialogueString = data.dialogueB;
+                break;
+            case NPCState.Finish:
+                dialogueString = data.dialogueC;
+                break;
+            default:
+                break;
+        }
+
+        for (int n = 0; n < dialogueString.Length; n++)
+        {
+            //print(data.dialogueA[n]);
+            textContent.text += dialogueString[n] + "";
+            yield return new WaitForSeconds(interval);
+
         }    
     }
 
