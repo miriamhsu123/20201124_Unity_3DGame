37d2e9f445279a6d5a82abb76bbcfa75d891d36d
miriamhsu123 <71687830+miriamhsu123@users.noreply.github.com>
2021-01-10 17:42:44 +0800

    怪物攻擊角色

--- New Unity Project/Assets/Scripts/Enemy.cs
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -9,6 +9,10 @@ public class Enemy : MonoBehaviour
     public float stopDistance = 2.5f;
     [Header("攻擊冷卻時間"), Range(0, 50)]
     public float CD = 2.3f;
+    [Header("攻擊中心點")]
+    public Transform ATKPoint;
+    [Header("攻擊長度"),Range(0f,5f)]
+    public float ATKLength;
 
     private Transform player;
     private NavMeshAgent nav;
@@ -33,6 +37,21 @@ public class Enemy : MonoBehaviour
     }
 
     /// <summary>
+    /// 繪製圖示事件:僅在Unity中顯示
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        //圖示.顏色=青色
+        Gizmos.color = Color.cyan;
+        //圖示.繪製射線(中心點，方向)
+        //(攻擊中心點的座標.攻擊中心點的前方*攻擊長度)
+        Gizmos.DrawRay(ATKPoint.position, ATKPoint.forward * ATKLength);
+    }
+
+    private RaycastHit hit;
+    
+    
+    /// <summary>
     /// 怪物攻擊角色
     /// </summary>
     private void Attack()
@@ -54,6 +73,14 @@ public class Enemy : MonoBehaviour
             {
                 ani.SetTrigger("攻擊觸發");
                 timer = 0;
+
+                //物理.射線碰撞(攻擊中心點的座標.攻擊中心點的前方,射線擊中的物件,攻擊長度)
+                //圖層:1<<圖層編號 = 只對8號圖層作用
+                //放在hit前的藍色out = 將資訊儲存於"hit"物件
+                if (Physics.Raycast(ATKPoint.position, ATKPoint.forward, out hit ,ATKLength, 1 << 8))
+                {
+                    hit.collider.GetComponent<Player>().Damage();
+                }
             }
         }
     }
---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Invector.vCharacterController; //引用套件 Invector.(腳本名稱)
+
+public class Player : MonoBehaviour
+{
+    private float hp = 100;  //將hp設定為100
+    private Animator ani;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    public void Damage()
+    {
+        hp -= 35;  //每一次攻擊損失35點hp
+        ani.SetTrigger("受傷觸發");  //受傷時播放"受傷觸發"動畫
+
+        //如果hp小於等於零，則執行Dead事件
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+    //創造名叫Dead的事件，內容為觸發"死亡觸發"動畫
+    private void Dead()
+    {
+        ani.SetTrigger("死亡觸發");
+
+        vThirdPersonController vt = GetComponent<vThirdPersonController>();
+        vt.lockMovement = true;
+        vt.lockRotation = true;
+    }
+}
